507d09c1436f4ce5b374d90645eb4773989f88ea
Justice Amoh <justiceamoh@gmail.com>
2015-09-28 17:07:55 -0400

    added ascii_ctc folder

---
+++ gen_ascii.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Description: command to generate ascii toy rnn data using python scripts from rakeshvar
+# Usage: scribe.py <out_file_name> [alphabet=ascii] [avg_sequence_len=20] [noise=0.1] [variable_length=True]
+
+output='data.pkl'
+alphabet='ascii'
+seqlen=22
+noise=0.08
+
+# python scribe.py data.pkl ascii
+python scribe.py $output $alphabet $seqlen $noise
